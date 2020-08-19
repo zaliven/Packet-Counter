77b82ea5662632a2b6f3f4d821a609ebbd67ecaf
zaliven <64747856+zaliven@users.noreply.github.com>
2020-08-19 23:51:10 +0300

    Update argparser.py

--- argparser.py
+++ argparser.py
@@ -5,6 +5,6 @@ def get_cli_args():
     parser = argparse.ArgumentParser(description="Algorithm Visualizer")
     parser.add_argument("-protocol", default='tcp', dest="protocol", help="Enter protocol to use (tcp)",
                         choices=["tcp"])
-    parser.add_argument("-port", default='443', dest="port", help="Enter port number",
+    parser.add_argument("-port", default='445', dest="port", help="Enter port number",
                         choices=['443', '445', '80'])
     return parser.parse_args()
